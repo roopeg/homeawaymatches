6b9d99b0eeeb74ad665f91e57da8028733914f1a
roopeg <roope.grohn@gmail.com>
2017-12-10 23:00:36 +0200

    The beginning

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+    function makeArrayA() {
+    var arrayA = $('.confA').val().split('\n');
+    return arrayA;
+  }
+    function makeArrayB() {
+    var arrayB = $('.confB').val().split('\n');
+
+    return arrayB;
+  }
+
+$("#submit").click(function(){
+  $('#output').text(makeArrayA());
+});
+
+function howManyHomeGames() {
+  var A = makeArrayA().length;
+  var B = makeArrayB().length;
+
+}
 
