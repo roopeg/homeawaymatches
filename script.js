c6cb590df918c4876be6b180da7106986a795108
roopeg <roope.grohn@gmail.com>
2017-12-11 00:10:16 +0200

    Adding some comments

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-    function makeArrayA() {
+    function makeArrayA() { // this creates array for conf A
     var arrayA = $('.confA').val().split('\n');
     return arrayA;
   }
-    function makeArrayB() {
+    function makeArrayB() { // this creates array for conf B
     var arrayB = $('.confB').val().split('\n');
 
     return arrayB;
   }
 
 $("#submit").click(function(){
-  $('#output').text(makeArrayA());
+  $('#output').text(makeArrayA()); // just a test to see if it works
 });
 
-function howManyHomeGames() {
+function howManyHomeGames() { // checks the number of teams in both confs
   var A = makeArrayA().length;
   var B = makeArrayB().length;
-
 }
-
